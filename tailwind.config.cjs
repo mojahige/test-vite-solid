c21114cc99caa1b4f00637b4cf040c2b91f0779d
mojahige <mojahige@gmail.com>
2021-05-25 22:37:29 +0900

    tailwindcss を使えるようにした

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./dev/index.html', './dev/src/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
