f746f261ad6b41beae0abcb18f486dd3f19ace84
mojahige <mojahige@gmail.com>
2021-05-29 16:28:12 +0900

    tailwindcss のテスト

--- dev/src/App.tsx
+++ dev/src/App.tsx
@@ -1,10 +1,12 @@
 import type { Component } from 'solid-js'
+import { Heading } from '@/Heading'
 import styles from './App.module.css'
 import image from '@assets/dummy.png'
 
 export const App: Component = () => {
   return (
     <>
+      <Heading textContent="Vite + Solid Test 👍" />
       <div className={styles.ImageContainer} data-testid="foo">
         <img src={`${image}`} className={styles.Image} alt="" />
       </div>
