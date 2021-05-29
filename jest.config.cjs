edce984f4ca24363b6fa2f428f3876230fd9951c
mojahige <mojahige@gmail.com>
2021-05-29 17:25:06 +0900

    moduleNameMapper の設定を修正

--- jest.config.cjs
+++ jest.config.cjs
@@ -85,8 +85,8 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.css$': 'identity-obj-proxy',
-    '^@/(.*)$': '<rootDir>/dev/src$1',
-    '^@assets/(.*)$': '<rootDir>/dev/assets$1',
+    '^@src/(.*)$': '<rootDir>/dev/src/$1',
+    '^@assets/(.*)$': '<rootDir>/dev/assets/$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
