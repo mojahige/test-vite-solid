c31547b16aac2d96d9c355d625259144447eba5d
mojahige <mojahige@gmail.com>
2021-05-29 17:46:30 +0900

    Heading コンポーネントのテスト追加

---
+++ dev/src/Heading.test.tsx
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom/extend-expect'
+import { render } from 'solid-testing-library'
+import { Heading } from './Heading'
+
+describe('Heading component', () => {
+  test('should render in the browser', () => {
+    const headingTextContent = 'Hello.'
+
+    const { getByText } = render(() => (
+      <Heading textContent={headingTextContent} />
+    ))
+
+    expect(getByText(headingTextContent)).toBeInTheDocument()
+  })
+})
