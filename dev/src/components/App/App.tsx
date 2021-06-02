d1b08ebf99003f818159fcc3b0758fb9f1943696
mojahige <mojahige@gmail.com>
2021-06-02 22:11:39 +0900

    不要コンポーネント削除

--- dev/src/components/App/App.tsx
+++ dev/src/components/App/App.tsx
@@ -1,6 +1,5 @@
 import type { Component } from 'solid-js';
 
-import { Heading } from '@src/components/Heading';
 import { EventNameForm } from '@src/components/EventNameForm';
 // import styles from './App.module.css'
 // import image from '@assets/dummy.png'
@@ -9,7 +8,6 @@ import { EventProvider } from '@src/stores/EventStore';
 export const App: Component = () => {
   return (
     <>
-      <Heading textContent="Vite + Solid Test 👍" />
       <EventProvider>
         <EventNameForm />
       </EventProvider>
--- dev/src/components/Heading/Heading.tsx
+++
@@ -1,9 +0,0 @@
-import type { Component } from 'solid-js';
-
-interface Props {
-  textContent: string;
-}
-
-export const Heading: Component<Props> = (props) => {
-  return <h1 className="text-5xl text-center">{props.textContent}</h1>;
-};
