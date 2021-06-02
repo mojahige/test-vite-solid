7c246a085fcac592da2d209cad4134633f5a4872
mojahige <mojahige@gmail.com>
2021-06-02 22:11:59 +0900

    run fix

--- dev/src/index.tsx
+++ dev/src/index.tsx
@@ -1,12 +1,12 @@
-import { render } from 'solid-js/web'
-import { App } from '@src/components/App'
+import { render } from 'solid-js/web';
+import { App } from '@src/components/App';
 
-import './index.css'
+import './index.css';
 
-const mountElement = document.getElementById('app')
+const mountElement = document.getElementById('app');
 
 if (mountElement) {
-  render(() => <App />, mountElement)
+  render(() => <App />, mountElement);
 } else {
-  console.error(`'mountElement' is not found...😭`)
+  console.error(`'mountElement' is not found...😭`);
 }
